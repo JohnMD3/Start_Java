9e27b6ca256110a6cadffbefe95888de7a11fae8
JohnMD3 <cernicaivan.md@gmail.com>
2023-05-29 19:03:15 +0300

    - HomeWorke - Added .gitignore

---
+++ src/HomeWorksL2/EX_9.java
@@ -0,0 +1,10 @@
+package HomeWorksL2;
+
+public class EX_9 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 1000000000; i += i)
+
+            System.out.println(i + " ");
+    }
+}
